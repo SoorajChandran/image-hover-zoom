3ebebfa2a1d0dd913d7f7c35a662b19d01b446fb
Sooraj Chandran <soorajchn@gmail.com>
2017-12-14 17:27:33 +0530

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import './App.css';
-import ImageZoom from './components/ImageZoom'
-
+import React from "react";
+import "./App.css";
+import ImageZoom from "./components/ImageZoom";
 
 const img1 = {
-        compressedImage: "https://images.pexels.com/photos/683940/pexels-photo-683940.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
-        originalImage: "https://static.pexels.com/photos/683940/pexels-photo-683940.jpeg",
-        zoomFactor: "3",
-        dir: "left"
-      }
+  compressedImage:
+    "https://images.pexels.com/photos/683940/pexels-photo-683940.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
+  originalImage:
+    "https://static.pexels.com/photos/683940/pexels-photo-683940.jpeg",
+  zoomFactor: "3",
+  dir: "left"
+};
 
 const img2 = {
-      compressedImage: "https://images.pexels.com/photos/681687/pexels-photo-681687.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
-      originalImage: "https://static.pexels.com/photos/681687/pexels-photo-681687.jpeg",
-      zoomFactor: "3",
-      dir: "right"
-    }
+  compressedImage:
+    "https://images.pexels.com/photos/681687/pexels-photo-681687.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
+  originalImage:
+    "https://static.pexels.com/photos/681687/pexels-photo-681687.jpeg",
+  zoomFactor: "3",
+  dir: "right"
+};
 
 const App = () => (
   <div className="App">
     <h1> Hover on the image </h1>
     <h1> React component for e-commerce product zoom on hover </h1>
-    <ImageZoom options={img1}/>
-    <ImageZoom options={img2}/>        
+    <ImageZoom options={img1} />
+    <ImageZoom options={img2} />
   </div>
-)
-
+);
 
 export default App;
